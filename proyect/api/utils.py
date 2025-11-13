a96631f2057023935511172b8099c1fbacad6174
Yagogoo39 <carlosmarcialdiego@gmail.com>
2025-11-13 15:01:15 -0600

    FinLogica

--- proyect/api/utils.py
+++ proyect/api/utils.py
@@ -1,27 +1,39 @@
 from antlr4 import *
 from lenguaje.GrammarLexer import GrammarLexer
 from lenguaje.GrammarParser import GrammarParser
-import io
-import sys
-import lenguaje.MyVisitor as MyVisitor
+#import lenguage.GrammarLexer as GrammarLexer
+#import lenguage.GrammarParser as GrammarParser
+import traceback
 
+import  io
+import sys
+#import lenguage.MyVisitor as MyVisitor
+from lenguaje.MyVisitor import MyVisitor
 
 def run_code(code:str):
-    input_stream = InputStream(code)
-    lexer = GrammarLexer(input_stream)
-    stream = CommonTokenStream(lexer)
-    parser = GrammarParser(stream)
-    tree = parser.program()
+    input_stream=InputStream(code)
+    lexer=GrammarLexer(input_stream)
+    stream=CommonTokenStream(lexer)
+    parser=GrammarParser(stream)
+    tree=parser.program()
 
-    #Capturan la salida
-    old_stdout = sys.stdout
+    # Capturan la salida 
+    old_stdout=sys.stdout
     buf = io.StringIO()
     sys.stdout = buf
-    #Creamos un objeto de nuestro visitor
-    visitor = MyVisitor()
-    #Visitamos el arbol con nuestro visitor
-    visitor.visit(tree)
-    #Capturamos la salida
-    output = buf.getvalue()
-
-    return output
+    
+    try:
+        #Creamos un objeto de nuestro visitor
+        visitor = MyVisitor()
+        #Visitamos el arbol con nuestro visitor
+        visitor.visit(tree)
+        # Capturamos la salida
+        output = buf.getvalue()
+        #Retornamos la salida de la operacion
+        return output
+    #Capturamos excepciones
+    except Exception:
+        tb = traceback.format_exc()
+        return tb
+    finally:
+        sys.stdout = old_stdout
--- proyect/lenguaje/MyVisitor.py
+++ proyect/lenguaje/MyVisitor.py
@@ -1,50 +1,81 @@
 from .GrammarVisitor import GrammarVisitor
+from .GrammarParser import GrammarParser
 
 class MyVisitor(GrammarVisitor):
+    # Definimos la memoria o el entorno
     def __init__(self):
-        self.memory={}
+        self.memory = { }
 
-#Definir la asignacion
-def visitAssign(self, ctx):
-    #Se obtiene el nombre de la variable y su valor
-    name=ctx.ID().getText()
-    #Se obtiene el valor, ya sea un numero o una expresion
-    value=self.visit(ctx.expr())
-    #Se almacena en la memoria a partir del nombre y el valor
-    self.memory[name]=value
-
-def visitPrint(self, ctx):
-    #Definimos la expresion que se desea mostrar
-    value=self.visit(ctx.expr())
-    #Imprimimos el valor en pantalla
-    print(value)
-
-#Definimos las expresiones
-def visitExpr(self, ctx):
-    #Busca si existen ID'S
-    if ctx.ID():
-        #Si el nombre de la variable no esta en memoria, lanza un error
-        name = ctx.ID().getText()
-        if name not in self.memory:
-            raise NameError(f"Variable '{name}' no definida")
-        # Si existe el nombre regresa la variable
-        return self.memory[name]
-    #Busca el operador numerico
-    elif ctx.op:
-        #Visita y obtiene lado izquierdo y derecho
-        left=self.visit(ctx.expr(0))
-        right=self.visit(ctx.expr(1))
-        #Evalua la operacion a realizar
-        if ctx.op.text=='+':
-            return left + right
-        if ctx.op.text=='-':
-            return left - right
-        if ctx.op.text=='*':
-            return left * right
-        if ctx.op.text=='/':
-            #Verifica que no se divida entre cero
-            if right == 0:
-                raise ValueError("Division por cero")
-            return left / right
+    # Definimos la asignacion
+    def visitAssing(self,ctx:GrammarParser.AssingContext):
+        # Se obtiene el tipo de la variable (int, string)
+        var_type = ctx.type_().getText()
+        # Se obtiene el id o nombre de la variable
+        name=ctx.ID().getText()
+        # Se obtiene el valor, ya sea un valor numerico o una expresion
+        value=self.visit(ctx.expr())
 
 
+        # Se almacena en memoria a partir del nombre y el valor
+        # self.memory[name]=value
+
+        # Validacion de tipos
+        if var_type == 'int' and not isinstance(value, int):
+            raise TypeError(f"Error en '{name}' ")
+        
+        if var_type == 'string' and not isinstance(value, str):
+            raise TypeError(f"Error en '{name}' ")
+
+        # Se almacena en memoria el valor y su tipo
+        self.memory[name] = {'value': value, 'type': var_type}
+
+
+    # Definimos la impresion
+    def visitPrint(self,ctx:GrammarParser.PrintContext):
+        # Definimos la expresion que se desea mostrar
+        value=self.visit(ctx.expr())
+        # Imprime el valor
+        print(value)
+
+    # Definimos las expresiones
+    def visitExpr(self, ctx):
+        # Busca si existen IDs
+        if ctx.ID():
+            # Obtiene del contexto el nombre de la variable
+            name=ctx.ID().getText()
+            # Si el nombre de la variable no esto, lanza un error
+            if name not in self.memory:
+                #raise NameError(f"Variable '{name}' no definida")
+                # Si existe el nombre retorna la variable
+                #return self.memory[name]
+                
+                raise NameError(f"Variable '{name}' no ha sido definida")
+            # Si existe el nombre retorna su valor
+            return self.memory[name]['value']
+        
+        # Busca si es un numero
+        elif ctx.NUMBER():
+            return int(ctx.NUMBER().getText())
+        # Busca si es un string
+        elif ctx.STRING():
+            # Retorna el texto dentro de las comillas
+            text = ctx.STRING().getText()
+            return text[1:-1]
+        # Busca el operador
+        elif ctx.op:
+            # Visita y obtiene lado izquierdo
+            left=self.visit(ctx.expr(0))
+            # Visita y obtiene lado derecho
+            right=self.visit(ctx.expr(1))
+            # Evalua la operacion a realizar
+            if ctx.op.text == "+":
+                return left + right
+            if ctx.op.text == "-":
+                return left - right
+            if ctx.op.text == "*":
+                return left * right
+            if ctx.op.text == "/":
+                #Verifica la division de cero
+                if right == 0:
+                    raise ValueError("Division por cero")
+                return left / right
